35648f578571ea88d31f372d4a1e0ac157dfc308
Shiven <shiven.sharma5111@gmail.com>
2026-02-27 02:44:53 +0000

    ProbA

--- Problems.py
+++ Problems.py
@@ -1,29 +1,8 @@
-cases = int(input("Enter number of cases: "))
+import sys
+import math
+import string
+
+cases = int(sys.stdin.readline().rstrip())
 
 for i in range(cases):
-    equation = input("Enter nums and sign (Example: 1 + 1): ")
-
-    parts = equation.split()   # splits into ["1", "+", "1"]
-
-    num1 = int(parts[0])
-    sign = parts[1]
-    num2 = int(parts[2])
-
-    if sign == "+":
-        result = num1 + num2
-        resultR = num2 + num1
-    elif sign == "-":
-        result = num1 - num2
-        resultR = num2 - num1
-    elif sign == "*":
-        result = num1 * num2
-        resultR = num2 * num1
-    elif sign == "/":
-        result = num1 / num2
-        resultR = num2 / num1
-    else:
-        print("Error")
-        continue
-
-    print("Result:", result)
-    print("Reverse Result:", resultR)
+    splitted = (sys.stdin.readline().rstrip()).split(" ")
