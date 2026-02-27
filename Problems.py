2e2e2e169ddc0fee0348a410d4820d0cf0c7ec26
Shiven <shiven.sharma5111@gmail.com>
2026-02-27 02:59:39 +0000

    ProbA

--- Problems.py
+++ Problems.py
@@ -1,19 +1,8 @@
-cases = int(input())
+import sys
+import math
+import string
+
+cases = int(sys.stdin.readline().rstrip())
 
 for i in range(cases):
-    line = input().strip()
-    V_str, X_str = line.split(":")
-    V = float(V_str)
-    X = float(X_str)
-
-    if V == 0:
-        print("SAFE")
-    else:
-        time = X / V
-
-        if time <= 1:
-            print("SWERVE")
-        elif time <= 5:
-            print("BRAKE")
-        else:
-            print("SAFE")
+    splitted = (sys.stdin.readline().rstrip()).split(" ")
