ccbeeb70ffe81f259d4c1c43bca960ebd42da76e
Shiven <shiven.sharma5111@gmail.com>
2026-02-28 01:59:56 +0000

    Prob14

--- hotinhere.py
+++ hotinhere.py
@@ -4,16 +4,19 @@ import math
 
 cases = int(sys.stdin.readline().rstrip()) 
 
-for caseNum in range(cases): 
-    temp = str(sys.stdin.readline().rstrip())
+for i in range(cases):
+    conversons = int(sys.stdin.readline().rstrip())
 
-    if "F" in temp:
-        tempC = float(temp.replace("F",""))
-        tempC2 = (5 / 9) * (tempC - 32)
-        rounded_tempC2 = round(tempC2, 1)
-        print(f"{temp} = {rounded_tempC2} C")
-    else:
-        tempF = float(temp.replace("C",""))
-        tempF2 = (tempF * (9 / 5)) + 32
-        rounded_tempF2 = round(tempF2, 1)
-        print(f"{temp} = {rounded_tempF2} F")
+    for conv in range(conversons): 
+        temp = str(sys.stdin.readline().rstrip())
+
+        if "F" in temp:
+            tempC = float(temp.replace("F",""))
+            tempC2 = (5 / 9) * (tempC - 32)
+            rounded_tempC2 = round(tempC2, 1)
+            print(f"{temp} = {rounded_tempC2} C")
+        else:
+            tempF = float(temp.replace("C",""))
+            tempF2 = (tempF * (9 / 5)) + 32
+            rounded_tempF2 = round(tempF2, 1)
+            print(f"{temp} = {rounded_tempF2} F")
